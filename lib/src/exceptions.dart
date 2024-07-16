4d2fbe1f2ddc068076c0b51879d455724f7537ed
kume1a <kumela011@gmail.com>
2024-07-16 18:01:30 +0400

    private req in progress

---
+++ lib/src/exceptions.dart
@@ -0,0 +1,215 @@
+class ClientError implements Exception {
+  ClientError({
+    this.response,
+    this.code,
+    this.message,
+  });
+
+  final dynamic response;
+  final dynamic code;
+  final String? message;
+
+  @override
+  String toString() {
+    return message ?? '';
+  }
+}
+
+class ClientUnknownError extends ClientError {}
+
+class WrongCursorError extends ClientError {
+  @override
+  String message = 'You specified a non-existent cursor';
+}
+
+class ClientStatusFail extends ClientError {}
+
+class ClientErrorWithTitle extends ClientError {}
+
+class ResetPasswordError extends ClientError {}
+
+class GenericRequestError extends ClientError {
+  GenericRequestError() : super(message: 'Sorry, there was a problem with your request');
+}
+
+class ClientGraphqlError extends ClientError {
+  ClientGraphqlError() : super(message: 'Raised due to graphql issues');
+}
+
+class ClientJSONDecodeError extends ClientError {
+  ClientJSONDecodeError() : super(message: 'Raised due to json decoding issues');
+}
+
+class ClientConnectionError extends ClientError {
+  ClientConnectionError() : super(message: 'Raised due to network connectivity-related issues');
+}
+
+class ClientBadRequestError extends ClientError {
+  ClientBadRequestError() : super(message: 'Raised due to a HTTP 400 response');
+}
+
+class ClientUnauthorizedError extends ClientError {
+  ClientUnauthorizedError() : super(message: 'Raised due to a HTTP 401 response');
+}
+
+class ClientForbiddenError extends ClientError {
+  ClientForbiddenError() : super(message: 'Raised due to a HTTP 403 response');
+}
+
+class ClientNotFoundError extends ClientError {
+  ClientNotFoundError() : super(message: 'Raised due to a HTTP 404 response');
+}
+
+class ClientThrottledError extends ClientError {
+  ClientThrottledError() : super(message: 'Raised due to a HTTP 429 response');
+}
+
+class ClientRequestTimeout extends ClientError {
+  ClientRequestTimeout() : super(message: 'Raised due to a HTTP 408 response');
+}
+
+class ClientIncompleteReadError extends ClientError {
+  ClientIncompleteReadError() : super(message: 'Raised due to incomplete read HTTP response');
+}
+
+class ClientLoginRequired extends ClientError {
+  ClientLoginRequired() : super(message: 'Instagram redirect to https://www.instagram.com/accounts/login/');
+}
+
+class ReloginAttemptExceeded extends ClientError {}
+
+class PrivateError extends ClientError {
+  PrivateError({String? message}) : super(message: message ?? 'For Private API and last_json logic');
+}
+
+class NotFoundError extends PrivateError {
+  String reason = 'Not found';
+}
+
+class FeedbackRequired extends PrivateError {}
+
+class ChallengeError extends PrivateError {}
+
+class ChallengeRedirection extends ChallengeError {}
+
+class ChallengeRequired extends ChallengeError {}
+
+class ChallengeSelfieCaptcha extends ChallengeError {}
+
+class ChallengeUnknownStep extends ChallengeError {}
+
+class SelectContactPointRecoveryForm extends ChallengeError {}
+
+class RecaptchaChallengeForm extends ChallengeError {}
+
+class SubmitPhoneNumberForm extends ChallengeError {}
+
+class LegacyForceSetNewPasswordForm extends ChallengeError {}
+
+class LoginRequired extends PrivateError {
+  LoginRequired() : super(message: 'Instagram request relogin');
+}
+
+class SentryBlock extends PrivateError {}
+
+class RateLimitError extends PrivateError {}
+
+class ProxyAddressIsBlocked extends PrivateError {
+  ProxyAddressIsBlocked()
+      : super(
+            message:
+                'Instagram has blocked your IP address, use a quality proxy provider (not free, not shared)');
+}
+
+class BadPassword extends PrivateError {}
+
+class BadCredentials extends PrivateError {}
+
+class PleaseWaitFewMinutes extends PrivateError {}
+
+class UnknownError extends PrivateError {}
+
+class TrackNotFound extends NotFoundError {}
+
+class MediaError extends PrivateError {}
+
+class MediaNotFound extends MediaError {}
+
+class StoryNotFound extends MediaError {}
+
+class UserError extends PrivateError {}
+
+class UserNotFound extends UserError {}
+
+class CollectionError extends PrivateError {}
+
+class CollectionNotFound extends CollectionError {}
+
+class DirectError extends PrivateError {}
+
+class DirectThreadNotFound extends DirectError {}
+
+class DirectMessageNotFound extends DirectError {}
+
+class VideoTooLongException extends PrivateError {}
+
+class VideoNotDownload extends PrivateError {}
+
+class VideoNotUpload extends PrivateError {}
+
+class VideoConfigureError extends VideoNotUpload {}
+
+class VideoConfigureStoryError extends VideoConfigureError {}
+
+class PhotoNotUpload extends PrivateError {}
+
+class PhotoConfigureError extends PhotoNotUpload {}
+
+class PhotoConfigureStoryError extends PhotoConfigureError {}
+
+class IGTVNotUpload extends PrivateError {}
+
+class IGTVConfigureError extends IGTVNotUpload {}
+
+class ClipNotUpload extends PrivateError {}
+
+class ClipConfigureError extends ClipNotUpload {}
+
+class AlbumNotDownload extends PrivateError {}
+
+class AlbumUnknownFormat extends PrivateError {}
+
+class AlbumConfigureError extends PrivateError {}
+
+class HashtagError extends PrivateError {}
+
+class HashtagNotFound extends HashtagError {}
+
+class LocationError extends PrivateError {}
+
+class LocationNotFound extends LocationError {}
+
+class TwoFactorRequired extends PrivateError {}
+
+class HighlightNotFound extends PrivateError {}
+
+class NoteNotFound extends NotFoundError {
+  @override
+  String reason = 'Not found';
+}
+
+class PrivateAccount extends PrivateError {
+  PrivateAccount() : super(message: 'This Account is Private');
+}
+
+class InvalidTargetUser extends PrivateError {
+  InvalidTargetUser() : super(message: 'Invalid target user');
+}
+
+class InvalidMediaId extends PrivateError {
+  InvalidMediaId() : super(message: 'Invalid media_id');
+}
+
+class MediaUnavailable extends PrivateError {
+  MediaUnavailable() : super(message: 'Media is unavailable');
+}
--- lib/src/prelogin_flow.dart
+++ lib/src/prelogin_flow.dart
@@ -1,25 +1,43 @@
 import 'dart:convert';
-import 'dart:ffi';
 import 'dart:math';
 import 'package:http/http.dart' as http;
-import 'package:uuid/uuid.dart';
+import 'dart:developer' as dev;
 
+import 'exceptions.dart';
 import 'util/util.dart';
 import 'util/uuid_factory.dart';
 
 class PreLoginFlow {
   PreLoginFlow(
-    this._uuidFactory,
-  ) {
+    this._uuidFactory, {
+    List<int>? delayRange,
+  }) {
     _uuid = _uuidFactory.v4;
+
+    _delayRange = delayRange ?? [1, 3];
   }
 
   final UuidFactory _uuidFactory;
+
+  late final List<int> _delayRange;
 
   final Map<String, String> _cookieDict = {};
   final Map<String, String> _authorizationData = {};
   String _uuid = '';
   String _token = '';
+  int _privateRequestsCount = 0;
+  dynamic _lastResponse;
+
+  Future<void> _randomDelay({
+    required List<int> delayRange,
+  }) {
+    final random = Random();
+    final minSleep = delayRange[0];
+    final maxSleep = delayRange[1];
+    final delay = Duration(seconds: minSleep + random.nextInt(maxSleep - minSleep));
+
+    return Future.delayed(delay);
+  }
 
   int? get userId {
     String? userId = _cookieDict['ds_user_id'];
@@ -41,24 +59,200 @@ class PreLoginFlow {
     return _token;
   }
 
+  String get authorization {
+    if (_authorizationData.isNotEmpty) {
+      String b64part = base64.encode(json.encode(_authorizationData).codeUnits);
+
+      return 'Bearer IGT:2:$b64part';
+    }
+    return '';
+  }
+
   Future<bool> preLoginFlow() async {
     await _syncLauncher(true);
 
     return true;
   }
 
-  Future<Map<String, dynamic>> _syncLauncher(bool login) async {
+  Future<Map<String, dynamic>?> _syncLauncher(bool login) async {
     Map<String, dynamic> data = {
       'id': _uuid,
       'server_config_retrieval': '1',
     };
 
-    if (login == false) {
+    if (!login) {
       data['_uid'] = userId;
       data['_uuid'] = _uuid;
       data['_csrftoken'] = token;
     }
 
-    return _privateRequest('launcher/sync/', data, login: login);
+    return _privateRequest(
+      'launcher/sync/',
+      data: data,
+      login: login,
+    );
   }
+
+  Future<Map<String, dynamic>?> _privateRequest(
+    String endpoint, {
+    Map<String, dynamic>? data,
+    Map<String, dynamic>? params,
+    bool login = false,
+    bool withSignature = true,
+    Map<String, String>? headers,
+    dynamic extraSig,
+    String? domain,
+  }) async {
+    if (authorization.isNotEmpty) {
+      headers ??= {};
+
+      if (!headers.containsKey('authorization')) {
+        headers['Authorization'] = authorization;
+      }
+    }
+    Map<String, dynamic> kwargs = {
+      'data': data,
+      'params': params,
+      'login': login,
+      'with_signature': withSignature,
+      'headers': headers,
+      'extra_sig': extraSig,
+      'domain': domain,
+    };
+    try {
+      if (_delayRange.isNotEmpty) {
+        await _randomDelay(delayRange: _delayRange);
+      }
+      _privateRequestsCount++;
+      return _sendPrivateRequest(
+        endpoint,
+        data: data,
+        params: params,
+        login: login,
+        withSignature: withSignature,
+        headers: headers,
+        extraSig: extraSig,
+        domain: domain,
+      );
+    } on ClientRequestTimeout {
+      dev.log('Wait 60 seconds and try one more time (ClientRequestTimeout)');
+      await Future.delayed(const Duration(seconds: 60));
+      return _sendPrivateRequest(
+        endpoint,
+        data: data,
+        params: params,
+        login: login,
+        withSignature: withSignature,
+        headers: headers,
+        extraSig: extraSig,
+        domain: domain,
+      );
+    } catch (e) {
+      dev.log('error sending private request $e');
+    }
+    return null;
+  }
+
+  // Future<Map<String, dynamic>> _sendPrivateRequest(
+  //   String endpoint, {
+  //   Map<String, dynamic>? data,
+  //   Map<String, dynamic>? params,
+  //   bool login = false,
+  //   bool withSignature = true,
+  //   Map<String, String>? headers,
+  //   dynamic extraSig,
+  //   String? domain,
+  // }) async {
+  //   _lastResponse = null;
+  //   Map<String, dynamic> lastJson = {};
+  //   _private.headers.addAll(_baseHeaders);
+  //   if (headers != null) {
+  //     _private.headers.addAll(headers);
+  //   }
+  //   if (!login) {
+  //     await Future.delayed(Duration(seconds: _requestTimeout));
+  //   }
+  //   try {
+  //     if (!endpoint.startsWith('/')) {
+  //       endpoint = '/v1/$endpoint';
+  //     }
+
+  //     if (endpoint == '/challenge/') {
+  //       endpoint = '/v1/challenge/';
+  //     }
+
+  //     String apiUrl = 'https://${domain ?? config.API_DOMAIN}/api$endpoint';
+  //     logger.info(apiUrl);
+  //     if (data != null) {
+  //       _private.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+  //       if (withSignature) {
+  //         data = generateSignature(json.encode(data));
+  //         if (extraSig != null) {
+  //           data += '&' + extraSig.join('&');
+  //         }
+  //       }
+  //       var response = await _private.post(
+  //         apiUrl,
+  //         body: data,
+  //         headers: _private.headers,
+  //         params: params,
+  //         proxies: _private.proxies,
+  //       );
+  //       logger.debug(
+  //         'private_request ${response.statusCode}: ${response.url} (${response.body})',
+  //       );
+  //       var mid = response.headers['ig-set-x-mid'];
+  //       if (mid != null) {
+  //         _mid = mid;
+  //       }
+  //       _requestLog(response);
+  //       _lastResponse = response;
+  //       response.raiseForStatus();
+  //       lastJson = json.decode(response.body);
+  //       logger.debug('last_json $lastJson');
+  //     } else {
+  //       _private.headers.remove('Content-Type');
+  //       var response = await _private.get(
+  //         apiUrl,
+  //         headers: _private.headers,
+  //         params: params,
+  //         proxies: _private.proxies,
+  //       );
+  //       logger.debug(
+  //         'private_request ${response.statusCode}: ${response.url} (${response.body})',
+  //       );
+  //       var mid = response.headers['ig-set-x-mid'];
+  //       if (mid != null) {
+  //         _mid = mid;
+  //       }
+  //       _requestLog(response);
+  //       _lastResponse = response;
+  //       response.raiseForStatus();
+  //       lastJson = json.decode(response.body);
+  //       logger.debug('last_json $lastJson');
+  //     }
+  //   } on FormatException catch (e) {
+  //     logger.error(
+  //       'Status ${_lastResponse.statusCode}: JSONDecodeError in private_request (user_id=$_userId, endpoint=$endpoint) >>> ${_lastResponse.body}',
+  //     );
+  //     throw ClientJSONDecodeError(
+  //       'JSONDecodeError $e while opening ${_lastResponse.url}',
+  //       response: _lastResponse,
+  //     );
+  //   } on http.ClientException catch (e) {
+  //     throw ClientConnectionError('${e.runtimeType} $e');
+  //   } on http.HTTPException catch (e) {
+  //     throw ClientError(e, response: _lastResponse, lastJson: lastJson);
+  //   } on http.TimeoutException catch (e) {
+  //     throw ClientRequestTimeout(e, response: _lastResponse, lastJson: lastJson);
+  //   } on http.Response catch (e) {
+  //     throw ClientError(e, response: _lastResponse, lastJson: lastJson);
+  //   }
+  //   if (lastJson.containsKey('status') && lastJson['status'] == 'fail') {
+  //     throw ClientError(response: _lastResponse, lastJson: lastJson);
+  //   } else if (lastJson.containsKey('error_title')) {
+  //     throw ClientError(response: _lastResponse, lastJson: lastJson);
+  //   }
+  //   return lastJson;
+  // }
 }
